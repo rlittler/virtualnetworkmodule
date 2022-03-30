31b26e6bdf2e5a56949094df2c799ed523918401
rlittler <36643307+rlittler@users.noreply.github.com>
2022-03-30 10:32:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  provider = var.hub_subcription
+  provider            = azurerm.hub_subscription
   count                                          = length(var.subnet_names)
   name                                           = var.subnet_names[count.index]
   resource_group_name                            = data.azurerm_resource_group.vnet.name
@@ -51,14 +51,14 @@ locals {
 }
 
 resource "azurerm_subnet_network_security_group_association" "vnet" {
-  provider = var.hub_subcription
+  provider            = azurerm.hub_subscription
   for_each                  = var.nsg_ids
   subnet_id                 = local.azurerm_subnets[each.key]
   network_security_group_id = each.value
 }
 
 resource "azurerm_subnet_route_table_association" "vnet" {
-  provider = var.hub_subcription
+  provider            = azurerm.hub_subscription
   for_each       = var.route_tables_ids
   route_table_id = each.value
   subnet_id      = local.azurerm_subnets[each.key]
